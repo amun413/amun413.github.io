a6d016cecd17751297d79c6b8440e48ffada1d02
Anna Mun <47487475+amun413@users.noreply.github.com>
2019-04-26 13:49:58 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,178 +1,169 @@
-// This Code Doesn't Looks So Good Sorry For That 
-// 😊
+console.clear();
 
-var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
+class Dots {
+  constructor(canvasId) {
+    this.dots = [];
+    this.canvas = document.getElementById(canvasId);
+    this.context = this.canvas.getContext("2d");
+    this.baseRadius = 6;
+    this.gap = this.baseRadius * 4;
+    this.balloon = this.baseRadius * 2.5;
+    this.areaOfInfluence = 300;
 
-canvas.height = innerHeight;
-canvas.width = innerWidth;
+    const init = this.init.bind(this);
+    const getRadii = this.getRadii.bind(this);
+    
+    init();
+    windowResize().listen(init);
+    
+    document.addEventListener("mousemove", e =>
+      requestAnimationFrame(() => getRadii(e))
+    );
+  }
 
-document.documentElement.style.overflow = 'hidden';
+  generateDots() {
+    const { canvas, gap } = this;
+    const r = this.baseRadius;
+    let dots = [];
 
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-// Declarations --------------------
-var mouse = {
-	x: 0,
-	y: 0
-};
-// Utilities ----------------------
+    for (let x = 0; x < canvas.width + r; x += gap) {
+      for (let y = 0; y < canvas.height + r; y += gap) {
+        dots.push({ x, y, r });
+      }
+    }
+    
+    return dots;
+  }
+  
+  scale (num, in_min, in_max, out_min, out_max) {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
 
-function randomIntFromRange(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+  setRadius(dot, e) {
+    const { areaOfInfluence, baseRadius, balloon } = this;
+    const { x, y } = dot;
+    const { clientX, clientY } = e;
+    const xDiff = Math.abs(x - clientX);
+    const yDiff = Math.abs(y - clientY);
+    const distance = Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
+
+    dot.r = baseRadius;
+
+    if (distance < areaOfInfluence) {
+      dot.r = this.scale(distance, 0, areaOfInfluence, balloon, baseRadius);
+    }
+
+    return dot;
+  }
+
+  redraw() {
+    const { canvas, context, dots } = this;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    dots.forEach(dot => {
+      const { x, y, r } = dot;
+      context.beginPath();
+      context.arc(x, y, r, 0, 2 * Math.PI);
+      context.fill();
+    });
+  }
+
+  getRadii(e) {
+    const setRadius = this.setRadius.bind(this);
+    this.dots = this.dots.map(dot => setRadius(dot, e));
+    this.redraw();
+  }
+
+  init() {
+    this.dots = this.generateDots();
+    this.redraw();
+  }
 }
 
+const dots = new Dots("dottest");
 
-// Bubbles -------------------------
-class Bubbles {
-	constructor(x, y, radius) {
-		this.x = x;
-		this.y = y;
-		this.radius = radius;
-		this.color = {
-			bg: 'rgba(255, 255, 255, 0.45)'
-		};
-		this.velocity = {
-			x: (Math.random() - 0.5) * 0.5,
-			y: Math.random() * 2
-		};
-		this.opacity = 1;
-	}
+/**
+ * Performant window resize event listener
+ * @return {object}
+ *
+ * @usage
+ *     import windowResize from './window-resize';
+ *     windowResize.listen(() => {
+ *         ...
+ *     });
+ */
+function windowResize() {
+  const windowResize = {
+    callbacks: [],
+    running: false,
+
+    /**
+     * Add the event listener
+     * @return {undefinied}
+     */
+    init() {
+      window.addEventListener("resize", windowResize.resize, {
+        passive: true
+      });
+    },
+
+    /**
+     * Loop through callbacks and run them
+     * @return {undefinied}
+     */
+    runCallbacks() {
+      windowResize.callbacks.forEach(callback => {
+        callback();
+      });
+
+      windowResize.running = false;
+    },
+
+    /**
+     * Event listener method
+     * @return {undefinied}
+     */
+    resize() {
+      if (!windowResize.running) {
+        windowResize.running = true;
+
+        if (window.requestAnimationFrame) {
+          window.requestAnimationFrame(() => windowResize.runCallbacks());
+        } else {
+          setTimeout(() => windowResize.runCallbacks(), 100);
+        }
+      }
+    },
+
+    /**
+     * Add a callback to the callbacks array
+     * @return {undefinied}
+     */
+    addCallback(callback) {
+      if (callback) {
+        windowResize.callbacks.push(callback);
+      }
+    },
+
+    /**
+     * Test whether or not any callbacks have been registered yet
+     * @return {Boolean}
+     */
+    hasCallbacks() {
+      return windowResize.callbacks.length > 0;
+    }
+  };
+
+  return {
+    // public method to add additional callback
+    listen: callback => {
+      if (!windowResize.hasCallbacks()) {
+        windowResize.init();
+      }
+      windowResize.addCallback(callback);
+    }
+  };
 }
-Bubbles.prototype.draw = function () {
-	c.beginPath();
-	c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-	c.fillStyle = this.color.bg;
-	c.fill();
-	c.closePath();
-};
-Bubbles.prototype.update = function () {
-	this.y -= this.velocity.y;
-	this.draw();
-};
-
-
-// Mini Bubbles ---------------
-
-class miniBubbles {
-	constructor(x, y, radius) {
-		this.x = x;
-		this.y = y;
-		this.radius = radius;
-		this.color = {
-			bg: 'rgba(255, 255, 255, 0.45)',
-		};
-		this.velocity = {
-			x: (Math.random() - 0.5) * 0.6,
-			y: (Math.random() - 1) * 0.5
-		}
-		this.gravity = -0.03;
-		this.timeToLive = 500;
-	}
-}
-miniBubbles.prototype.draw = function () {
-	c.save();
-	c.beginPath();
-	c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-	c.fillStyle = this.color.bg;
-	c.fill();
-	c.closePath();
-	c.restore();
-}
-miniBubbles.prototype.update = function () {
-	if (this.y - this.radius < 0) {
-		this.velocity.y = this.velocity.y;
-	} else {
-		this.velocity.y += this.gravity;
-	}
-	this.x += this.velocity.x;
-	this.y += this.velocity.y;
-	this.timeToLive -= 1;
-	this.draw();
-}
-
-const backgroundGradient = c.createLinearGradient(0, 0, 0, canvas.height);
-backgroundGradient.addColorStop(0, '#009cff')
-backgroundGradient.addColorStop(1, '#007bff')
-var bubbles = [];
-var minibubbles = [];
-var timer = 0;
-var spawnRate = 99;
-
-function init() {
-	bubbles = [];
-	minibubbles = [];
-}
-
-function animate() {
-	requestAnimationFrame(animate);
-    //background linear gradient ------------
-    c.fillStyle = backgroundGradient;
-    c.fillRect(0, 0, canvas.width, canvas.height);
-
-	// Render the Bubbles -------------------
-	bubbles.forEach((bubble, index) => {
-		bubble.update();
-		if (bubble.radius == 0) {
-			bubbles.splice(index, 1);
-		}
-	});
-	minibubbles.forEach((minibubble, index) => {
-		minibubble.update();
-		if (minibubble.timeToLive == 0) {
-			minibubbles.splice(index, 1);
-		}
-	});
-
-	timer++;
-	if (timer % spawnRate == 0) {
-		const radius = randomIntFromRange(15, 30);
-		const minradius = Math.random() * 2 + 1;
-		// const radius = 15;
-		const x = Math.max(radius, Math.random() * canvas.width - radius);
-		const y = innerHeight + 100;
-		bubbles.push(new Bubbles(x, y, radius, 'white'));
-		minibubbles.push(new miniBubbles(x, y, minradius));
-		spawnRate = randomIntFromRange(70, 200);
-	}
-
-
-	//  When Hover over the bubbles ------------------
-	for (var i = 0; i < bubbles.length; i++) {
-		if (
-			mouse.x > bubbles[i].x - bubbles[i].radius &&
-			mouse.x < bubbles[i].x + bubbles[i].radius
-		) {
-			if (
-				mouse.y > bubbles[i].y - bubbles[i].radius &&
-				mouse.y < bubbles[i].y + bubbles[i].radius
-			) {
-				var x = bubbles[i].x;
-				var y = bubbles[i].y;
-				var radius = Math.random() * 2 + 1;
-				bubbles[i].radius -= bubbles[i].radius;
-				for (let a = 0; a < Math.random() * 4 + 4; a++) {
-					minibubbles.push(new miniBubbles(x, y, radius));
-				}
-			}
-		}
-	}
-}
-
-
-// Event Listener ---------------
-
-canvas.addEventListener('mousemove', mouseMove);
-
-function mouseMove(e) {
-	mouse.x = e.offsetX;
-	mouse.y = e.offsetY;
-}
-addEventListener('resize', function () {
-	canvas.height = innerHeight;
-	canvas.width = innerWidth;
-	init();
-});
-
-//  call ---------------
-animate();
-init();
